e836ceb8c421b753613bdf07e44f0943c215fa07
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-07-04 22:50:38 +0300

    task form added

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -1,8 +1,36 @@
-
+import { useState } from "react";
 const AddTaskForm = () => {
+  const [task, setTask] = useState("");
+  const [day, setDay] = useState("");
   return (
-    <div>AddTaskForm</div>
-  )
-}
+    <div className="form-control">
+      <form action="">
+        <div>
+          <label htmlFor="task">TASK</label>
+          <input
+            type="text"
+            name="task"
+            id="task"
+            placeholder="add task"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="task">TASK</label>
+          <input
+            type="date"
+            name="day"
+            id="day"
+            // placeholder="Day & Time"
+            value={day}
+            onChange={(e) => setDay(e.target.value)}
+          />
+        </div>
+        <div><button className="btn btn-submit" type="submit">Submit</button></div>
+      </form>
+    </div>
+  );
+};
 
-export default AddTaskForm
+export default AddTaskForm;
