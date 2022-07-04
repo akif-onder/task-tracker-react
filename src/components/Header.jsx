84b29993f0a27bdf11ab2f2f75925adb35f88307
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-07-04 22:42:42 +0300

    continued

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,34 @@
+import AddTaskForm from "./AddTaskForm";
+import { useState } from "react";
+
 const Header = () => {
-  return <header>Header</header>;
+
+  const [show, setShow] = useState(false);
+  const [btnStyle, setBtnStyle] = useState({name:"SHOW ADD TASK BAR", bgcolor:"purple"})
+
+  const handleShow = () => {
+
+    if (show) {
+      setBtnStyle({
+        name:"SHOW ADD TASK BAR", bgcolor:"purple"
+      })
+    }else{
+      setBtnStyle({
+        name:"SHOW ADD TASK BAR", bgcolor:"red"
+      });
+
+    };
+    setShow(!show);
+  };
+  console.log(show);
+
+  return (
+  <header className="header">
+    <h1>TASK TRACKER</h1>
+    <button onClick={handleShow} className="btn" style={{backgroundColor: btnStyle.bgcolor}}>{btnStyle.name}</button>
+    {show && <AddTaskForm/>}  
+</header>
+  )
 };
 
 export default Header;
